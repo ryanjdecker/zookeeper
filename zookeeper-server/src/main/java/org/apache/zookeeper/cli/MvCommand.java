8b465e35ca21a1e041af158e2bbdeb7249387457
Jason Lim <44080259+jsjlim27@users.noreply.github.com>
2023-06-01 20:15:51 -0700

    cleaned up

--- zookeeper-server/src/main/java/org/apache/zookeeper/cli/MvCommand.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/cli/MvCommand.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.zookeeper.cli;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -30,51 +12,28 @@ import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Stat;
 import org.apache.zookeeper.server.EphemeralType;
-import org.apache.zookeeper.util.*;
+import org.apache.zookeeper.util;
 
 /**
  * mv command for cli
  */
 public class MvCommand extends CliCommand {
-
         private static Options options = new Options();
         private String[] args;
         private CommandLine cl;
 
-        static {
-                options.addOption(new Option("i", false, "interactive"));
-                options.addOption(new Option("v", false, "verbose"));
-        }
-
-        public MvCommand() {
-                super("mv", "[-i] [-v] source dest");
-        }
-
-        @Override
-        public CliCommand parse(String[] cmdArgs) throws CliParseException {
-                DefaultParser parser = new DefaultParser();
-                try {
-                        cl = parser.parse(options, cmdArgs);
-                } catch (ParseException ex) {
-                        throw new CliParseException(ex);
-                }
-                args = cl.getArgs();
-                if (args.length < 3) {
-                        throw new CliParseException(getUsageStr());
-                }
-                return this;
-        }
-
         @Override
         public boolean exec() throws CliException {
-                boolean hasI = cl.hasOption("i");
-                boolean hasV = cl.hasOption("v");
+                String src = args[1];
+                String dest = args[2];
 
-                TransportTreeFactory treeFactory = new SimpleTransportTreeFactory(source);
-                TransportTree tt = treeFactory.makeNewTree();
+                TransportTreeExtractor tte = new TransportTreeExtractor(zk);
+                TransportRecoupler ttr = new TransportTreeRecoupler(zk);
+
+                TransportTree tt = tte.extractTree(src);
+
+                ttr.attachTree(dest, tt);
 
                 return true;
         }
-
-
 }
